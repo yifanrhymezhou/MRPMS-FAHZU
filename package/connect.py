0624e25b5cecf52e444681884f92f3063aeafe8f
Yifan (Rhyme) Zhou <107736520+yifanrhymezhou@users.noreply.github.com>
2022-08-09 07:09:12 +0800

    Update connect.py

--- package/connect.py
+++ package/connect.py
@@ -1,16 +1,11 @@
 import pymssql
-""" connect = pymssql.connect(server='60.12.15.28', port='20014',
-		user='sa',
-		password='Excelgits',
-		database='projmgn',
-		charset='cp936')
-#format restful api's result (json): from list -> dictionary 
-cursor = connect.cursor(as_dict=True) """
+""" SQL Server's sensitive info erased """
+"""format restful api's result (json): from list -> dictionary: cursor = connect.cursor(as_dict=True) """
 
 def get_db(sql, charset, action):
-	connect = pymssql.connect(server='60.12.15.28', port='20014',
-		user='sa',
-		password='Excelgits',
+	connect = pymssql.connect(server='**.**.**.**', port=''*****',
+		user='**',
+		password='*****',
 		database='projmgn',
 		charset=charset,
 		as_dict=True)
@@ -29,13 +24,3 @@ def get_db(sql, charset, action):
 	finally:
 		cursor.close()
 		connect.close()
-		
-
-
-
-""" connect2 = pymssql.connect(server='60.12.15.28', port='20014',
-		user='sa',
-		password='Excelgits',
-		database='projmgn',
-		charset='utf8')
-cursor2 = connect2.cursor(as_dict=True) """
